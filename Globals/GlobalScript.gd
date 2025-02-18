ffd8282d7e2efa5c0675675c679dd2777e7b8bae
Grahamet1999 <grahamet1999@gmail.com>
2025-02-18 11:06:45 -0500

    Added wire minigame and some global scripts

---
+++ Globals/GlobalScript.gd
@@ -0,0 +1,39 @@
+extends Node
+
+@onready var level = 1
+@onready var numPuzzlesLeft = 0
+@onready var numPuzzlesPerLevel = [2,7,13]
+@onready var levelComplete = false
+@onready var introComplete = true
+@onready var levelStarted = false
+
+func _ready() -> void:
+	Signals.connect("wireGameCompleted", _on_wire_game_completed)
+
+func _physics_process(delta: float) -> void:
+	
+	if levelStarted == false:
+		match level:
+			0:
+				numPuzzlesLeft = 0
+			1: 
+				numPuzzlesLeft = numPuzzlesPerLevel[0]
+				levelStarted = true
+			2: 
+				numPuzzlesLeft = numPuzzlesPerLevel[1]
+				levelStarted = true
+			3: 
+				numPuzzlesLeft = numPuzzlesPerLevel[2]
+				levelStarted = true
+				
+	if numPuzzlesLeft == 0:
+		levelComplete = true
+		
+	if levelComplete == true:
+		level += 1
+	pass
+
+# Game complete functions
+func _on_wire_game_completed(state : bool):
+	numPuzzlesLeft -= 1
+	print("Wire Game completed successfully")
--- Levels/Props/test_prop.gd
+++ Levels/Props/test_prop.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 @onready var playerNode = get_tree().get_first_node_in_group("Player")
 @onready var playerID = playerNode.get_instance_id()
 @onready var detectionArea = get_node("DetectionArea")
+@onready var root = $".".get_parent()
+@onready var solved = false
+
+const wireMiniGame = preload("res://Levels/Mini-games/WireMiniGame.tscn")
 
 var entered = false
 var interactedWith = false
@@ -19,11 +23,13 @@ func _physics_process(delta: float) -> void:
 	pass
 	
 	
-
 func _on_interact(state : bool, objectId : int):
-	if state == true and objectId == self.objectID:
+	if state == true and objectId == self.objectID and self.solved == false:
 		print("Interacted with: " + str(self.objectID))
-
+		var wireGameInstance = wireMiniGame.instantiate()
+		self.add_child(wireGameInstance)
+		Signals.inMiniGame.emit(true)
+		
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("Player"):
 		entered = true
