e684ccfb05ee7dfe9bb9e0a40bf2ce595be42f93
World <virk1noff@gmail.com>
2025-02-22 22:13:41 -0500

    Music/Sound and Credits scene
    Music/Sound and Credits scene

--- Levels/Menus/OOT.gd
+++ Levels/Menus/OOT.gd
@@ -5,13 +5,16 @@ extends Control
 @onready var main_menu = $VBoxContainer/Main_menu as Button
 @export var main_menu_scene: String = "res://Levels/Menus/main_menu.tscn"
 @onready var exit_btn = $VBoxContainer/Exit_btn as Button
-
+@onready var animated_texture = preload("res://Assets/Props/AnimatedBackground.tres")
+@onready var credits_btn = $VBoxContainer/Credits as Button
 
 func _ready() -> void:
 	main_menu.grab_focus()
 	get_node("/root/MusicManager").play_music()
 	main_menu.connect("pressed",_on_main_menu_pressed)
 	exit_btn.connect("pressed",_on_exit_btn_pressed)
+	credits_btn.connect("pressed",_on_credits_pressed)
+	$TextureRect.texture = animated_texture
 	 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file(main_menu_scene)
@@ -19,3 +22,8 @@ func _on_main_menu_pressed() -> void:
 
 func _on_exit_btn_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/Menus/GodotCredits.tscn")
